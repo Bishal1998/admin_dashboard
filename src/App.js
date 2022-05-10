01fd660dc87d768de4d17e71ecc8d455007eba6a
Bishal1998 <Bishalpaudel1998@gmail.com>
2022-05-10 17:01:08 +0545

    file structure and react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
-
+import Home from "./pages/home/Home";
+import Login from './pages/login/Login';
+import Single from './pages/single/Single';
+import List from './pages/list/List';
+import New from './pages/new/New';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
